c844aa794f392092ff3aba2741dde87b8b586888
jayce <jslesar@beta.team>
2021-10-28 12:23:31 -0400

    povurty

--- eda.py
+++ eda.py
@@ -24,6 +24,7 @@ vermont = df[df['State'] == 'Vermont']
 
 vt_counties = vermont['County'].unique()
 condensed_vermont = vermont.groupby('County').sum()
+mean_poverty = vermont.groupby('County').mean()['PovertyRate']
 
 
 with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
@@ -41,14 +42,15 @@ colorscale = ["#f7fbff", "#ebf3fb", "#deebf7", "#d2e3f3", "#c6dbef", "#b3d2e9", 
     "#85bcdb", "#6baed6", "#57a0ce", "#4292c6", "#3082be", "#2171b5", "#1361a9",
     "#08519c", "#0b4083", "#08306b"]
 
-
 values = condensed_vermont['Pop2010'].values
-print(sum(values))
+values = mean_poverty.round(3).values
+endpoints = sorted([int(val) for val in values])
+
 fig = ff.create_choropleth(
     fips=fips, values=values, scope=['VT'],
-    binning_endpoints=sorted([int(val) for val in values]), colorscale=colorscale,
+    binning_endpoints=[], colorscale=colorscale,
     county_outline={'color': 'rgb(255,255,255)', 'width': 0.5}, round_legend_values=True,
-    legend_title='Population by County', title='Vermont'
+    legend_title='Poverty Rate by County', title='Vermont'
 )
 fig.layout.template = None
 fig.show()
