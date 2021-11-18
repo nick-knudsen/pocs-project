8037770935cabef7855162786fd0e1345f14f97e
jayce <jslesar@beta.team>
2021-11-18 18:09:01 -0500

    example

--- data_viz.py
+++ data_viz.py
@@ -3,44 +3,30 @@ import plotly.figure_factory as ff
 import plotly.graph_objs as go
 
 
-df, counties, fips = prep_vermont()
-
-
 colorscale = ["#f7fbff", "#ebf3fb", "#deebf7", "#d2e3f3", "#c6dbef", "#b3d2e9", "#9ecae1",
     "#85bcdb", "#6baed6", "#57a0ce", "#4292c6", "#3082be", "#2171b5", "#1361a9",
     "#08519c", "#0b4083", "#08306b"]
 
+def gen_choropleth(values: list, title: str, legend_title: str, fips: list, colorscale=colorscale):
 
-mean_poverty = df.groupby('County').mean()['PovertyRate']
+    fig = ff.create_choropleth(
+        fips=fips, values=values, scope=['VT'],
+        binning_endpoints=[], colorscale=colorscale,
+        county_outline={'color': 'rgb(255,255,255)', 'width': 0.5}, round_legend_values=True,
+        legend_title=legend_title, title=title
+    )
 
-
-values = mean_poverty.round(3).values
-
-fig = ff.create_choropleth(
-    fips=fips, values=values, scope=['VT'],
-    binning_endpoints=[], colorscale=colorscale,
-    county_outline={'color': 'rgb(255,255,255)', 'width': 0.5}, round_legend_values=True,
-    legend_title='Poverty Rate by County', title='Vermont'
-)
+    return fig
 
 
-fig.add_trace(go.Scattergeo(
-            lon = [44.467500],
-            lat = [-73.175630],
-            text = ['test McDonalds'],
-            name = 'fast food',
-            mode = 'markers',
-            marker = dict(
-                size = 20,
-                color = '#08306b',
-                line_width = 0,
-                sizeref = 9,
-                sizemode = "area",
-                reversescale = True
-            )))
+df, counties, fips = prep_vermont()
 
 
-fig.update_geos(fitbounds="locations")
+condensed_counties = df.groupby('County').mean()
 
 
-fig.show()
+mean_poverty = condensed_counties['PovertyRate']
+mean_pop = condensed_counties['Pop2010']
+
+poverty_map = gen_choropleth(mean_poverty, 'Mean Poverty', 'Mean Poverty Levels', fips)
+pop_map = gen_choropleth(mean_pop, 'Mean Pop', 'Mean Population Levels', fips)
